8077e5a2731d96e0252ca42cc0e75e4d3b19c994
Eoin Houlihan <ehoulih@tcd.ie>
2016-11-11 15:37:40 +0000

    Add back in the demo page

--- src/Main.hs
+++ src/Main.hs
@@ -4,6 +4,8 @@ import Text.Blaze.Svg.Renderer.Utf8 (renderSvg)
 import Web.Scotty
 
 import Render
+import Shapes
+import StyleSheet
 
 svg drawing = do
   setHeader "Content-Type" "image/svg+xml"
@@ -13,6 +15,11 @@ svg drawing = do
 main :: IO ()
 main = scotty 3000 $ do
   get "/" $ file "static/index.html"
+
+  get "/demo" $ svg $ render [(identity, circle, defaultStyle),
+                              (translate 3 4 <+> scale 10 10 <+> rotate 10,
+                               square,
+                               defaultStyle <:> strokeWidth 2 <:> stroke green <:> fill (custom 250 124 124))]
 
   post "/draw" $ do
     drawing <- jsonData
