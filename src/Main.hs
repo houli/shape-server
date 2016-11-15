d9ea1a61c676f85648a5bf344612ef684f701a81
Eoin Houlihan <ehoulih@tcd.ie>
2016-11-15 19:20:24 +0000

    Add skews for the craic

--- src/Main.hs
+++ src/Main.hs
@@ -16,7 +16,7 @@ main :: IO ()
 main = scotty 3000 $ do
   get "/" $ file "static/index.html"
 
-  get "/demo" $ svg $ render [(identity, circle, defaultStyle),
+  get "/demo" $ svg $ render [(skewX 30, circle, defaultStyle),
                               (translate 3 4 <+> scale 10 10 <+> rotate 10,
                                square,
                                defaultStyle <:> strokeWidth 2 <:> stroke green <:> fill (custom 250 124 124))]
--- src/Shapes.hs
+++ src/Shapes.hs
@@ -12,6 +12,8 @@ module Shapes
   , translate
   , rotate
   , scale
+  , skewX
+  , skewY
   , (<+>)
   , transform
   ) where
@@ -43,6 +45,8 @@ data Transform = Identity
                | Translate Double Double
                | Scale Double Double
                | Rotate Double
+               | SkewX Double
+               | SkewY Double
                | Compose Transform Transform
                deriving Show
 
@@ -53,12 +57,16 @@ instance FromJSON Transform where
       "translate" -> Translate <$> o .: "x" <*> o .: "y"
       "scale"     -> Scale <$> o .: "x" <*> o .: "y"
       "rotate"    -> Rotate <$> o .: "angle"
+      "skewX"     -> SkewX <$> o .: "angle"
+      "skewY"     -> SkewY <$> o .: "angle"
       _           -> fail ("Invalid transform \"" ++ transformType ++ "\"")
 
 identity = Identity
 translate = Translate
 scale = Scale
 rotate = Rotate
+skewX = SkewX
+skewY = SkewY
 t0 <+> t1 = Compose t0 t1
 
 -- Composition of transforms is matrix multiplication
@@ -66,9 +74,16 @@ transform :: Transform -> M.Matrix Double
 transform Identity = M.identity 3
 transform (Translate x y) = M.fromLists [[1, 0, x], [0, 1, y], [0, 0, 1]]
 transform (Scale x y) = M.fromLists [[x, 0, 0], [0, y, 0], [0, 0, 1]]
-transform (Rotate b) = let a = (b * pi) / 180 in -- convert degrees to radians
+transform (Rotate b) = let a = degreesToRadians b in -- convert degrees to radians
                          M.fromLists [[cos a, -(sin a), 0], [sin a, cos a, 0], [0, 0, 1]]
+transform (SkewX b) = let a = degreesToRadians b in
+                        M.fromLists [[1, tan a, 0], [0, 1, 0], [0, 0, 1]]
+transform (SkewY b) = let a = degreesToRadians b in
+                        M.fromLists [[1, 0, 0], [tan a, 1, 0], [0, 0, 1]]
 transform (Compose t1 t2) = transform t1 `M.multStd2` transform t2
+
+degreesToRadians :: Double -> Double
+degreesToRadians x = (x * pi) / 180
 
 type Drawing = [(Transform, Shape, StyleSheet)]
 
